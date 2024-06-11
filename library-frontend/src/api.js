0e34cfac94047db8ea76bd31bfd19b9911623c95
userpeaseonly <userpeaseonly@gmail.com>
2024-06-11 16:03:26 +0500

    Frontend and Backend sites are now working fine

--- library-frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-// src/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:8000', // Adjust the base URL according to your backend setup
-});
-
-export const getBooks = () => api.get('/bookse/');
-export const deleteBook = (id) => api.delete(`/bookse/${id}/`);
